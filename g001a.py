7e99903863e1ebb55b8ee1d4c29e9119ae0ce5bb
partiadam <hunadam2004@gmail.com>
2020-11-03 09:34:23 +0000

    print('kérek egy számot') szám1 = input() egésszám1 = int(szám1) print('kérek egy másik számot') szám2 = input() egésszám2 = int(szám2) szorzat = egészszám1 * egészszám2 print(szorzat)

--- g001a.py
+++ g001a.py
@@ -1,1 +1,8 @@
-print ("Szia") 
+print('kérek egy számot')
+szám1 = input()
+egésszám1 = int(szám1)
+print('kérek egy másik számot')
+szám2 = input()
+egésszám2 = int(szám2)
+szorzat = egészszám1 * egészszám2
+print(szorzat)
